48637e6a24f0faf55f1273c27330cebf27169e9f
YangTzuHung <j25889651556@gmail.com>
2022-03-20 17:19:55 +0800

    202203201719

--- src/scripts/Config.js
+++ src/scripts/Config.js
@@ -1,5 +1,5 @@
 const Config = {
-    ROSBRIDGE_SERVER_IP: "10.10.10.89",
+    ROSBRIDGE_SERVER_IP: "10.10.10.109",
     ROSBRIDGE_SERVER_PROT: "9090",
     RECONNECTION_TIMER: 3000,
     //CMD_VEL_TOPIC: "/turtle1/cmd_vel", //操控輸出 for turtle
